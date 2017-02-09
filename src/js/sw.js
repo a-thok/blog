77eefbe9936aaf18ff02f9fb528143fde2e04176
Victor <a-thok@outlook.com>
2017-02-09 17:42:33 +0800

    启用service worker

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,54 +1,67 @@
-const CACHE_NAME = 'A-TALK-TO-ME-v1';
+const CACHE_NAME = 'A-TALK-TO-ME-V1';
 const urlsToCache = [
   '/',
+  '/lib/prism.css',
+  '/lib/prism.js',
   '/css/style.css',
-  '/css/prism.css',
   '/js/speak.js',
-  '/js/prism.js',
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches
+      .open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-
-        const request = event.request.clone();
-        return fetch(request)
-          /* eslint-disable no-shadow */
-          .then((response) => {
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          });
-      })
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys()
-      .then(cacheNames => Promise.all(
-        cacheNames
-          .filter(cacheName => cacheName !== CACHE_NAME)
-          .map(cacheName => caches.delete(cacheName))
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
       ))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  // should only intercept GET requests
+  if (request.method !== 'GET') {
+    return event.respondWith(fetch(request));
+  }
+
+  const cacheTheResponse = (res) => {
+    // don't need to cache response if the request is failed
+    if (!res || res.status !== 200 || res.type !== 'basic') {
+      return res;
+    }
+
+    const clonedRes = res.clone();
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        cache.put(request, clonedRes);
+      });
+    return res;
+  };
+
+  return event.respondWith(
+    caches.match(request)
+      .then((cachedRes) => {
+        const networkedRes = fetch(request)
+          .then(cacheTheResponse)
+          // TODO: return an offline page when there is no cache nor network
+          .catch(() => new Response('', {
+            status: 503,
+            statusText: 'Service Unavailable',
+          }));
+
+        // will return cache if there is one, otherwise it will return the normal request result
+        return cachedRes || networkedRes;
+      })
   );
 });
