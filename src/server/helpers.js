192650087d40699289aa1dc4f30e27e9ed876d75
Victor <a-thok@outlook.com>
2017-02-22 14:17:50 +0800

    添加Content-Security-Policy

--- src/server/helpers.js
+++ src/server/helpers.js
@@ -1,3 +1,56 @@
+const uuid = require('uuid');
+const csp = require('helmet-csp');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+const directives = {
+  blockAllMixedContent: true,
+  upgradeInsecureRequests: true,
+  defaultSrc: ["'self'"],
+  styleSrc: [
+    "'self'",
+    'fonts.googleapis.com',
+    'a.disquscdn.com',
+  ],
+  scriptSrc: [
+    "'self'",
+    'cdn.polyfill.io',
+    'cdn.bootcss.com',
+    'atalktome.disqus.com',
+    'a.disquscdn.com',
+    (req, res) => `'nonce-${res.locals.nonce}'`,
+  ],
+  fontSrc: [
+    "'self'",
+    'fonts.gstatic.com',
+  ],
+  connectSrc: [
+    "'self'",
+    'ws:',
+    'disqus.com/api',
+    'links.services.disqus.com/api',
+  ],
+  frameSrc: [
+    "'self'",
+    'disqus.com',
+  ],
+};
+
+if (isDev) {
+  directives.styleSrc.push('blob:');
+  directives.scriptSrc.push("'unsafe-eval'");
+}
+
+export const setNonce = (req, res, next) => {
+  res.locals.nonce = uuid.v4();
+  return next();
+};
+
+export const setCsp = csp({
+  directives,
+  reportOnly: isDev,
+});
+
 export const setSecurityHeaders = (req, res, next) => {
   res.set({
     'X-Content-Type-Options': 'nosniff',
