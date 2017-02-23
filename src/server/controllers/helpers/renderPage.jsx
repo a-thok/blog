7700885d0679d801cd846c47a16fead1a0c65823
Victor <a-thok@outlook.com>
2017-02-23 10:52:03 +0800

    优化IE下的样式兼容性

--- src/server/controllers/helpers/renderPage.jsx
+++ src/server/controllers/helpers/renderPage.jsx
@@ -13,19 +13,20 @@ const template = ({ nonce, title, app, initialState, isIE }) =>
   <title>${title}</title>
   <meta name="description" content="阿托的个人博客，记录技术生涯的成长与日常生活的琐碎">
   <meta name="author" name="阿托">
-  <!--[if lt IE 9]>
-    <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <![endif]-->
-  <!--[if lt IE 10]>
-    <script nonce=${nonce}> document.documentElement.className ='ie'; </script>
-  <![endif]-->
   <link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,700,700i" rel="stylesheet">
   <link href="/style.css" rel="stylesheet">
+  <!--[if lt IE 10]>
+  <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <link href="/ie.css" rel="stylesheet">
+  <![endif]-->
+  <!--[if lt IE 9]>
+  <link href="/ie8.css" rel="stylesheet">
+  <![endif]-->
   <script nonce=${nonce}>
     var __INITIAL_STATE__ = ${JSON.stringify(initialState)}
   </script>
   <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=Object.assign,Promise,fetch,Map,WeakMap" defer></script>
-  <script src="/app.js" defer></script>${isIE ? `\n<script src="//cdn.bootcss.com/svg4everybody/2.1.4/svg4everybody.legacy.min.js"></script>
+  <script src="/app.js" defer></script>${isIE ? `\n  <script src="//cdn.bootcss.com/svg4everybody/2.1.4/svg4everybody.legacy.min.js"></script>
   <script nonce=${nonce}> svg4everybody(); </script>` : ''}
 </head>
 <body>
