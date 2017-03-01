1f7a6b95d862c8f4b041c2421cde322e869ed929
Victor <a-thok@outlook.com>
2017-03-01 11:48:05 +0800

    添加加载动画效果及其它细节优化

--- src/client/views/Sentence.jsx
+++ src/client/views/Sentence.jsx
@@ -1,20 +1,29 @@
 /** @jsx Inferno */
 import Inferno from 'inferno';
 import Component from 'inferno-component';
+import Spinner from '../components/Spinner';
 
 class Sentence extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetching: false,
+    };
+  }
+
   componentDidMount() {
     document.title = 'A Talk To Me';
   }
 
   getSentence() {
+    this.setState({ fetching: true });
+
     fetch('/?json=true')
       .then(res => res.json())
       .then(({ success, result }) => {
         if (success) {
-          console.log(1);
-          console.log(result);
           this.props.update(result);
+          this.setState({ fetching: false });
         }
       });
   }
@@ -23,9 +32,13 @@ class Sentence extends Component {
     return (
       <div className="sentence">
         <blockquote className="sentence-quote">
-          {this.props.sentence}
+          {this.state.fetching ? <Spinner /> : this.props.sentence}
         </blockquote>
-        <button className="btn sentence-btn" onClick={() => this.getSentence()}>随便说说</button>
+        <button
+          className="btn sentence-btn"
+          title="点击切换上方句子"
+          onClick={() => this.getSentence()}
+        >随便说说</button>
       </div>
     );
   }
