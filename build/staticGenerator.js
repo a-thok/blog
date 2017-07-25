ca3cb4c04b66f9bfec8961884f45bfeb6b1b89b5
Victor <a-thok@outlook.com>
2017-07-25 11:58:24 +0800

    调整目录结构

--- build/staticGenerator.js
+++ build/staticGenerator.js
@@ -14,7 +14,7 @@ const readFileAsPromise = filename => new Promise((resolve, reject) => {
   });
 });
 
-const POST_DIR_PATH = path.resolve('src/posts');
+const POST_DIR_PATH = path.resolve('db/posts');
 const filenames = fs.readdirSync(POST_DIR_PATH);
 
 Promise.all(
